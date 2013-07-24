b73088349c2beb888a93e99861cfb7f491933db0
Shiv Kumar <shivpkumar@gmail.com>
2013-07-23 23:36:44 -0700

    testing pony gem for mailer

--- shivnatra-mailer.rb
+++ shivnatra-mailer.rb
@@ -1,6 +1,13 @@
+require 'rubygems'
 require 'sinatra'
 require 'json'
+require 'pony'
 
 post '/' do
-  email_data = JSON.parse(request.body.read)
+  data = JSON.parse(request.body.read, symbolize_names: true)
+  data[:from] = 'noreply@example.com'
+  puts "this is your data: #{data}"
+  Pony.mail(data)
 end
+
+# { "to": "shivpkumar@gmail.com", "subject": "hello world", "body": "Hi John! Sending you an email via this awesome API I just made on the interwebs." }
