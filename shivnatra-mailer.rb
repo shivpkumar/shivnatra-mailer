0a2b4075c4ba7c92892dfa1fd8603f25df376b3f
Shiv Kumar <shivpkumar@gmail.com>
2013-07-24 09:15:02 -0700

    adjusted Pony method call to reflect SendGrid changes

--- shivnatra-mailer.rb
+++ shivnatra-mailer.rb
@@ -5,6 +5,18 @@ require 'pony'
 
 post '/' do
   data = JSON.parse(request.body.read, symbolize_names: true)
-  data[:from] = 'noreply@example.com'
-  Pony.mail(data)
+  Pony.mail(to: data[:to],
+            from: 'noreply@example.com',
+            subject: data[:subject],
+            body: data[:body],
+            :via => :smtp,
+            via_options: {
+              address: 'smtp.sendgrid.net',
+              port: '587',
+              domain: 'heroku.com',
+              user_name: ENV['SENDGRID_USERNAME'],
+              password: ENV['SENDGRID_PASSWORD'],
+              :authentication => :plain,
+              enable_starttls_auto: true
+            })
 end
